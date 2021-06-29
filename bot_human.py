210c247f41e79da3074846dc2c6f1d6835733fef
josevenzke <josevenzkejr@gmail.com>
2021-06-29 17:55:52 -0300

    fully working for memory test

--- bot_human.py
+++ bot_human.py
@@ -1,7 +1,20 @@
 from selenium import webdriver
+import time
 
 browser = webdriver.Chrome('C:/Users/José/Downloads/chromedriver_win32/chromedriver.exe')
 browser.get('https://humanbenchmark.com/tests/verbal-memory')
 elem = browser.find_element_by_class_name('css-de05nr')
 elem.click()
 
+seen = []
+
+while True:
+    time.sleep(0.1)
+    word = browser.find_element_by_class_name('word').text
+    print(word)
+    if word in seen:
+        browser.find_element_by_xpath('//*[@id="root"]/div/div[4]/div[1]/div/div/div/div[3]/button[1]').click()
+    else:
+        browser.find_element_by_xpath('//*[@id="root"]/div/div[4]/div[1]/div/div/div/div[3]/button[2]').click()
+        seen.append(word)
+    
