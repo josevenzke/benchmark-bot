8f419f5a2b1eda3088af7af5c7b813e5dbfcf3d7
josevenzke <josevenzkejr@gmail.com>
2021-06-29 17:44:47 -0300

    opens benchmark page, and starts game

--- bot_human.py
+++ bot_human.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://seleniumhq.org/')
+browser = webdriver.Chrome('C:/Users/José/Downloads/chromedriver_win32/chromedriver.exe')
+browser.get('https://humanbenchmark.com/tests/verbal-memory')
+elem = browser.find_element_by_class_name('css-de05nr')
+elem.click()
+
